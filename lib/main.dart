05b474b0de7823b7b204a252613c3a0c843937dd
G1Joshi <joshijeevan98@gmail.com>
2021-04-10 23:26:05 +0530

    add xylophone tile

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,28 +7,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Home(),
+      title: 'Xylophone',
+      home: MyHome(),
     );
   }
 }
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter'),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Tile(color: Colors.red, number: 1),
+            Tile(color: Colors.orange, number: 2),
+            Tile(color: Colors.yellow, number: 3),
+            Tile(color: Colors.green, number: 4),
+            Tile(color: Colors.blue, number: 5),
+            Tile(color: Colors.indigo, number: 6),
+            Tile(color: Colors.purple, number: 7),
+          ],
+        ),
       ),
-      body: Center(),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  Tile({required this.color, required this.number});
+
+  final Color color;
+  final int number;
+
+  void play(int number) => AudioCache().play('assets/note$number.wav');
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => play(number),
+        child: Container(
+          color: color,
+        ),
+      ),
     );
   }
 }
